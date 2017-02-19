2e137df5b8297668774ffda5f5b6a24dec136c38
krustagi <krustagi@purdue.edu>
2017-02-19 15:58:23 -0500

    added postback and a few message cases

--- index.js
+++ index.js
@@ -35,8 +35,17 @@ app.post('/webhook/', function (req, res) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
         if (event.message && event.message.text) {
-            let text = event.message.text
+            text = event.message.text
+            //let text = event.message.text
+            if (text == 'hi') {
+                //do something
+            }
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+        if (event.postback) {
+            text = JSON.stringify(event.postback)
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+            continue
         }
     }
     res.sendStatus(200)
